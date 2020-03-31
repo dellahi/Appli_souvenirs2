2a8b6e68b4fd9199ea5453150560f7ca337cf5a3
Manuel Jéhanno <manuel.jehanno.etu@univ-lemans.fr>
2020-03-31 16:41:02 +0200

    mini modifs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'fil-souvenir', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'resultat-recherche',
