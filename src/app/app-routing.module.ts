ef3577b29ae1dbd3bd8784992c3bb76edcf039fe
Manuel Jéhanno <manuel.jehanno.etu@univ-lemans.fr>
2020-03-31 15:13:18 +0200

    link partager souvenir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'fil-souvenir', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'resultat-recherche',
