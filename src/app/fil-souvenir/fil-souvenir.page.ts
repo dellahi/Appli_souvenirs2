f786fbf8c154613a78bb5a6b21d21f93857564e2
Juliane Muller <ju.mullerg@gmail.com>
2020-03-31 16:13:57 +0200

    DerniereVersionAlignee v2

--- src/app/fil-souvenir/fil-souvenir.page.ts
+++ src/app/fil-souvenir/fil-souvenir.page.ts
@@ -8,10 +8,10 @@ import{Souvenir} from '../../app/fil-souvenir/souvenirs';
 })
 export class FilSouvenirPage implements OnInit {
 
-  listSouvenirs: Souvenir[] = [{nom:"Moi", lieu:"Presqu'ile de Quiberon", date:"02/09/2016", image:"assets/example2.jpg" }, 
-                                {nom:"Ellana" , lieu:"Maison" , date:"17/09/2015", image:"assets/livre.jpg"},
-                                {nom:"Moi", lieu:"Port blanc Quiberon", date:"28/07/2018", image:"assets/example.JPG"},
-                                {nom:"Framboisine", lieu:"J'aime les framboises, c'est quand même vachement bon", date:"17/02/2015", image:"assets/concert.jpg" }];
+  listSouvenirs: Souvenir[] = [{nom:"Moi", lieu:"Presqu'ile de Quiberon", date:"02/09/2016", image:"../assets/example2.jpg" }, 
+                                {nom:"Ellana" , lieu:"Maison" , date:"17/09/2015", image:"../assets/livre.jpg"},
+                                {nom:"Moi", lieu:"Port blanc Quiberon", date:"28/07/2018", image:"../assets/example.JPG"},
+                                {nom:"Framboisine", lieu:"J'aime les framboises, c'est quand même vachement bon", date:"17/02/2015", image:"../assets/concert.jpg" }];
 
   constructor() { }
 
--- src/app/recherche-profil/recherche-profil.page.ts
+++ src/app/recherche-profil/recherche-profil.page.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RechercheProfilPage implements OnInit {
 
+  listSouvenir: Souvenir[] =  [{nom:"Parachute", lieu:"Quiberon", date:"25/03/2018", image:"../../assets/example.jpg" }, 
+  {nom:"Séjour" , lieu:"Paris" , date:"10/10/2010", image:"../../assets/louvre.jpg"},
+  {nom:"Voyage", lieu:"Bretagne", date:"01/12/2018", image:"../../assets/example2.jpg"},
+  {nom:"Soirée film", lieu:"Confinement", date:"20/03/2020", image:"../../assets/eragon.jpg" }];
   constructor() { }
 
   ngOnInit() {
